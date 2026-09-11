ff97976ce7feef6e5626a5165581bde0473d8ccb
Namatashabira <ssematasabira24@gmail.com>
2026-09-11 22:36:36 +0300

    Stop tracking backend/ in root repo (it has its own git repo now)

--- backend/products/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Product, ProductReview, ReviewImage
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(ProductReview)
-admin.site.register(ReviewImage)
