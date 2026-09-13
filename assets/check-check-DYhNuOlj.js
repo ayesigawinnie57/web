ed60e49bf5d4a68deed79e382d80131aba3a7148
SSEMATA <SSEMATA@users.noreply.github.com>
2026-09-13 12:21:18 +0000

    deploy: ccdaf1fde0952499429e70d93fe84f4086513562

---
+++ assets/check-check-DYhNuOlj.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-B33jzjx3.js";var t={name:`check-check`,size:24,node:[[`path`,{d:`M18 6 7 17l-5-5`,key:`116fxf`}],[`path`,{d:`m22 10-7.5 7.5L13 16`,key:`ke71qq`}]]};t.node;var n=e(t);export{n as t};
